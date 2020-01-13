4127552827d1328de70543fb92472e9b35cd519d
Mike Spadaru <maspadaru@tutanota.com>
2020-01-13 15:26:37 +0100

    full implementation of cbparser.py

--- scripts/cbconvert.sh
+++ scripts/cbconvert.sh
@@ -3,22 +3,22 @@
 mkdir -p ../../cbscenarios-star/deep/100/data/
 mkdir -p ../../cbscenarios-star/deep/100/dependencies/
 python cbparse.py S 5 ../../chasebench/scenarios/deep/100/data/ > ../../cbscenarios-star/deep/100/data/input.stream
-python cbparse.py T 3 20 20 ../../chasebench/scenarios/deep/100/dependencies/ > ../../cbscenarios-star/deep/100/dependencies/program.star
+python cbparse.py T 2 50 50 50 ../../chasebench/scenarios/deep/100/dependencies/ > ../../cbscenarios-star/deep/100/dependencies/program.star
 
 mkdir -p ../../cbscenarios-star/correctness/tgds5/data/
 mkdir -p ../../cbscenarios-star/correctness/tgds5/dependencies/
 python cbparse.py S 5 ../../chasebench/scenarios/correctness/tgds5/data/ > ../../cbscenarios-star/correctness/tgds5/data/input.stream
-python cbparse.py T 3 20 20 ../../chasebench/scenarios/correctness/tgds5/dependencies/ > ../../cbscenarios-star/correctness/tgds5/dependencies/program.star
+python cbparse.py T 2 50 50 100 ../../chasebench/scenarios/correctness/tgds5/dependencies/ > ../../cbscenarios-star/correctness/tgds5/dependencies/program.star
 
 mkdir -p ../../cbscenarios-star/correctness/weak/data/
 mkdir -p ../../cbscenarios-star/correctness/weak/dependencies/
 python cbparse.py S 5 ../../chasebench/scenarios/correctness/weak/data/ > ../../cbscenarios-star/correctness/weak/data/input.stream
-python cbparse.py T 3 20 20 ../../chasebench/scenarios/correctness/weak/dependencies/ > ../../cbscenarios-star/correctness/weak/dependencies/program.star
+python cbparse.py T 2 50 50 100 ../../chasebench/scenarios/correctness/weak/dependencies/ > ../../cbscenarios-star/correctness/weak/dependencies/program.star
 
 mkdir -p ../../cbscenarios-star/correctness/tgds/data/
 mkdir -p ../../cbscenarios-star/correctness/tgds/dependencies/
-python cbparse.py S 5 ../../chasebench/scenarios/correctness/tgds/data/ > ../../cbscenarios-star/correctness/tgds/data/input.stream
-python cbparse.py T 3 20 20 ../../chasebench/scenarios/correctness/tgds/dependencies/ > ../../cbscenarios-star/correctness/tgds/dependencies/program.star
+python cbparse.py S 2 ../../chasebench/scenarios/correctness/tgds/data/ > ../../cbscenarios-star/correctness/tgds/data/input.stream
+python cbparse.py T 3 50 50 100 ../../chasebench/scenarios/correctness/tgds/dependencies/ > ../../cbscenarios-star/correctness/tgds/dependencies/program.star
 
 
 
--- scripts/cbparse.py
+++ scripts/cbparse.py
@@ -1,12 +1,9 @@
-'''
-Doc
-
-'''
-
 import string
 import sys
 import os
 
+OPERATOR_BOX = "B"
+OPERATOR_DIAMOND = "D"
 
 def print_help():
     print ('Usage: python cbparse.py Option Arguments DirectoryPath')
@@ -14,7 +11,7 @@ def print_help():
     print ('    S: parse chasebench data dir - Source Instance')
     print ('        Arguments: timeline_size')
     print ('    T: parse chasebench dependecies dir - TGDs')
-    print ('        Arguments: win_size percent_box percent_diam')
+    print ('        Arguments: window_size percent_box percent_diamond')
     print (' ')
     print ('Example: python cbparse.py D 1000 chasebench/scenarios/deep/100/data > out.stream')
     print ('Example: python cbparse.py T 3 20 50 chasebench/scenarios/deep/100/dependencies > out.laser')
@@ -74,12 +71,21 @@ def parse_source(timeline_size, dir_path):
 class Dependency:
     def __init__(self, line):
         self._parse_line(line)
+        self._existential_variables = self._get_existential_variables()
+        self.is_existential = len(self._existential_variables) > 0
+        self._events = []
+
 
     def __str__(self):
-        head = ' && '.join(self._head)
+        head_atoms = self._head[:]
+        for event in self._events:
+            event_atom = "[I, %s]" % (event) 
+            head_atoms.append(event_atom)
+        head = ' && '.join(head_atoms)
         body = ' && '.join(self._body) 
         rule = head + ' := ' + body
         return rule
+
 
     def _parse_line(self, line):
         line = line.replace('.','').replace('?','').replace(' ','')
@@ -88,6 +94,7 @@ class Dependency:
         head = line_list[1]
         self._body = self._parse_conjunction(body) 
         self._head = self._parse_conjunction(head) 
+
 
     def _parse_conjunction(self, conjunction):
         '''
@@ -109,25 +116,138 @@ class Dependency:
             result.append(atom)
         return result
 
+    
+    def _get_variable_list(self, atom):
+        atom = atom.replace(')', '')
+        variables = atom.split('(')[1]
+        var_list = variables.split(',')
+        return var_list
 
-def parse_tgds(win_size, percent_box, percent_diam, dir_path):
-    dependencies = []
-    for filename in os.listdir(dir_path):
-        if filename.endswith("st-tgds.txt") or filename.endswith("t-tgds.txt"): 
-            full_path = "%s%s" % (dir_path, filename)
-            with open(full_path) as openfileobject:
-                for line in openfileobject:
-                    line = line.rstrip()
-                    if line:    
-                        dependency = Dependency(line)
-                        dependencies.append(dependency)
-    '''
-       TODO: add window box or window diamond
-       TODO: add event variables 
-    '''
-    for dependency in dependencies:
-        print(dependency)
 
+    def _get_existential_variables(self):
+        body_variables = set()
+        head_variables = set()
+        for atom in self._body:
+            variables = self._get_variable_list(atom)
+            body_variables.update(variables)
+        for atom in self._head:
+            variables = self._get_variable_list(atom)
+            head_variables.update(variables)
+        existential_variables = head_variables.difference(body_variables)
+        return list(existential_variables)
+
+
+    def add_operator(self, operator, window_size):
+        new_body = []
+        for atom in self._body:
+            new_atom = "[$, %d] [%s] %s" % (window_size, operator, atom)
+            new_body.append(new_atom)
+        self._body = new_body
+
+    def add_event(self):
+        if self.is_existential:
+            self._events.append(self._existential_variables[0])
+
+    def add_all_events(self):
+        if self.is_existential:
+            self._events = self._existential_variables[:]
+
+
+class DependencyParser:
+    def __init__(self, window_size, percent_box, percent_diamond, \
+            percent_event, dir_path):
+        self._window_size = window_size
+        self._percent_box = percent_box
+        self._percent_diamond = percent_diamond
+        self._percent_event = percent_event
+        self._dir_path = dir_path 
+        self._st_dependencies = []
+        self._t_dependencies = []
+
+    def _parse_dependecy_file(self, file_name):
+        dependencies = []
+        full_path = "%s%s" % (self._dir_path, file_name)
+        with open(full_path) as openfileobject:
+            for line in openfileobject:
+                line = line.rstrip()
+                if line:    
+                    dependency = Dependency(line)
+                    dependencies.append(dependency)
+        return dependencies
+    
+
+    def _parse_tgds(self):
+        for file_name in os.listdir(self._dir_path):
+            if file_name.endswith(".st-tgds.txt"): 
+                new_deps = self._parse_dependecy_file(file_name)
+                self._st_dependencies.extend(new_deps)
+            if file_name.endswith(".t-tgds.txt"): 
+                new_deps = self._parse_dependecy_file(file_name)
+                self._t_dependencies.extend(new_deps)
+
+
+
+    def _count_existential_tgds(self, dependencies):
+        result = 0  
+        for dependency in dependencies:
+            if dependency.is_existential:
+                result += 1
+        return result 
+    
+
+    def _add_windows(self, dependencies):
+        number_tgds = len(dependencies)
+        number_box = (number_tgds * self._percent_box) / 100
+        number_diamond = (number_tgds * self._percent_diamond) / 100
+        for dependency in dependencies:
+            if number_box > 0:
+                dependency.add_operator(OPERATOR_BOX, self._window_size)
+                number_box -= 1
+            elif number_diamond > 0:
+                dependency.add_operator(OPERATOR_DIAMOND, self._window_size)
+                number_diamond -= 1
+
+
+    def _add_events(self, dependencies):
+        number_existential_tgds = self._count_existential_tgds(dependencies)
+        number_event = (number_existential_tgds * self._percent_event) / 100
+        for dependency in dependencies:
+            if number_event > 0 and dependency.is_existential:
+                # dependency.add_event()
+                dependency.add_all_events()
+                number_event -= 1
+
+
+    def _mutate_list(self, dependencies):
+        self._add_windows(dependencies)
+        self._add_events(dependencies)
+
+
+    def read(self):
+        self._parse_tgds()
+
+
+    def mutate(self):
+        self._mutate_list(self._st_dependencies)
+        self._mutate_list(self._t_dependencies)
+
+
+    def write(self):
+        for dependency in self._st_dependencies:
+            print(dependency)
+        for dependency in self._t_dependencies:
+            print(dependency)
+
+
+def validate_percentages(percent_box, percent_diamond, percent_event):
+    if percent_box > 100 or percent_diamond > 100 or percent_event > 100:
+        print("Percentages should not be larger than 100")
+        return False
+    if percent_box + percent_diamond > 100:
+        print("Percentages for box and diamond should not add up to more "\
+                "than 100")
+        return False
+    return True
 
 
 def main():
@@ -142,12 +262,19 @@ def main():
                 source_instance = parse_source(timeline_size, dir_path)
                 print_source(timeline_size, source_instance)
         elif option == 'T': 
-            if (len(sys.argv) == 6):
-                win_size = int(sys.argv[2])
+            if (len(sys.argv) == 7):
+                window_size = int(sys.argv[2])
                 percent_box = int(sys.argv[3])
-                percent_diam = int(sys.argv[4])
-                dir_path = validate_dir(sys.argv[5])
-                parse_tgds(win_size, percent_box, percent_diam, dir_path)
+                percent_diamond = int(sys.argv[4])
+                percent_event = int(sys.argv[5])
+                if validate_percentages(percent_box, percent_diamond, \
+                        percent_event):
+                    dir_path = validate_dir(sys.argv[6])
+                    parser = DependencyParser(window_size, percent_box, \
+                            percent_diamond,  percent_event, dir_path)
+                    parser.read()
+                    parser.mutate()
+                    parser.write()
         else:
             print_help()
 
