bdd5e61d36942004d0f8bc1535a76994593b3462
Mike Spadaru <maspadaru@tutanota.com>
2020-01-14 14:26:52 +0100

    added code to convert and run all scenarios

---
+++ cb_skolem_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ============================================================================
+./star.sh b
+# ============================================================================
+
+./star.sh r S ../cbscenarios-star/deep/100/dependencies/program.star ../cbscenarios-star/deep/100/data/input.stream 
+./star.sh r S ../cbscenarios-star/deep/200/dependencies/program.star ../cbscenarios-star/deep/200/data/input.stream 
+./star.sh r S ../cbscenarios-star/deep/300/dependencies/program.star ../cbscenarios-star/deep/300/data/input.stream 
+
+
+./star.sh r S ../cbscenarios-star/LUBM/dependencies/program.star ../cbscenarios-star/LUBM/data/001/input.stream 
+./star.sh r S ../cbscenarios-star/LUBM/dependencies/program.star ../cbscenarios-star/LUBM/data/010/input.stream 
+./star.sh r S ../cbscenarios-star/LUBM/dependencies/program.star ../cbscenarios-star/LUBM/data/01k/input.stream 
+./star.sh r S ../cbscenarios-star/LUBM/dependencies/program.star ../cbscenarios-star/LUBM/data/100/input.stream 
+
--- cbrun.sh
+++ cbrun.sh
@@ -5,4 +5,13 @@
 # ============================================================================
 
 ./star.sh r R ../cbscenarios-star/deep/100/dependencies/program.star ../cbscenarios-star/deep/100/data/input.stream 
+./star.sh r R ../cbscenarios-star/deep/200/dependencies/program.star ../cbscenarios-star/deep/200/data/input.stream 
+./star.sh r R ../cbscenarios-star/deep/300/dependencies/program.star ../cbscenarios-star/deep/300/data/input.stream 
+
+
+./star.sh r R ../cbscenarios-star/LUBM/dependencies/program.star ../cbscenarios-star/LUBM/data/001/input.stream 
+./star.sh r R ../cbscenarios-star/LUBM/dependencies/program.star ../cbscenarios-star/LUBM/data/010/input.stream 
+./star.sh r R ../cbscenarios-star/LUBM/dependencies/program.star ../cbscenarios-star/LUBM/data/01k/input.stream 
+./star.sh r R ../cbscenarios-star/LUBM/dependencies/program.star ../cbscenarios-star/LUBM/data/100/input.stream 
+
 
