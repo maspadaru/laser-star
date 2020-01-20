75221d1c4003ff2acaf5dfa9ee2f2a0d133b9f81
Mike Spadaru <maspadaru@tutanota.com>
2020-01-20 10:54:53 +0100

    scripts

--- cbrun.sh
+++ cbrun.sh
@@ -4,46 +4,67 @@
 ./star.sh b
 # ============================================================================
 
-#./star.sh r S ../cbscenarios-star/deep/100/dependencies/program.star ../cbscenarios-star/deep/100/data/input.stream 
-#./star.sh r S ../cbscenarios-star/deep/200/dependencies/program.star ../cbscenarios-star/deep/200/data/input.stream 
-#./star.sh r S ../cbscenarios-star/deep/300/dependencies/program.star ../cbscenarios-star/deep/300/data/input.stream 
-#./star.sh r R ../cbscenarios-star/deep/100/dependencies/program.star ../cbscenarios-star/deep/100/data/input.stream 
-#./star.sh r R ../cbscenarios-star/deep/200/dependencies/program.star ../cbscenarios-star/deep/200/data/input.stream 
-#./star.sh r R ../cbscenarios-star/deep/300/dependencies/program.star ../cbscenarios-star/deep/300/data/input.stream 
+PROGRAM="../cbscenarios-star/deep/100/dependencies/program.star"
+INPUT="../cbscenarios-star/deep/100/data/input.stream"
+
+PARAM="20 1 50 50 100"
+echo "STAR --- $PARAM"
+scripts/cbconvert.sh $PARAM
+./star.sh r R $PROGRAM $INPUT
+
+PARAM="20 1 50 50 0"
+echo "STAR --- $PARAM"
+scripts/cbconvert.sh $PARAM
+./star.sh r R $PROGRAM $INPUT
+
+PARAM="20 3 50 50 100"
+echo "STAR --- $PARAM"
+scripts/cbconvert.sh $PARAM
+./star.sh r R $PROGRAM $INPUT
+
+PARAM="20 3 50 50 0"
+echo "STAR --- $PARAM"
+scripts/cbconvert.sh $PARAM
+./star.sh r R $PROGRAM $INPUT
+
+PARAM="20 5 50 50 100"
+echo "STAR --- $PARAM"
+scripts/cbconvert.sh $PARAM
+./star.sh r R $PROGRAM $INPUT
+
+PARAM="20 5 50 50 0"
+echo "STAR --- $PARAM"
+scripts/cbconvert.sh $PARAM
+./star.sh r R $PROGRAM $INPUT
 
 
-#./star.sh r S ../cbscenarios-star/LUBM/dependencies/program.star ../cbscenarios-star/LUBM/data/001/input.stream 
-#./star.sh r R ../cbscenarios-star/LUBM/dependencies/program.star ../cbscenarios-star/LUBM/data/001/input.stream 
 
-#./star.sh r S ../cbscenarios-star/LUBM/dependencies/program.star ../cbscenarios-star/LUBM/data/010/input.stream 
-#./star.sh r R ../cbscenarios-star/LUBM/dependencies/program.star ../cbscenarios-star/LUBM/data/010/input.stream 
+PARAM="20 1 50 50 100"
+echo "STAR --- $PARAM"
+scripts/cbconvert.sh $PARAM
+./star.sh r S $PROGRAM $INPUT
 
+PARAM="20 1 50 50 0"
+echo "STAR --- $PARAM"
+scripts/cbconvert.sh $PARAM
+./star.sh r S $PROGRAM $INPUT
 
-#./star.sh r R ../cbscenarios-star/LUBM/dependencies/program.star ../cbscenarios-star/LUBM/data/100/input.stream 
+PARAM="20 3 50 50 100"
+echo "STAR --- $PARAM"
+scripts/cbconvert.sh $PARAM
+./star.sh r S $PROGRAM $INPUT
 
-# ============================================================================
+PARAM="20 3 50 50 0"
+echo "STAR --- $PARAM"
+scripts/cbconvert.sh $PARAM
+./star.sh r S $PROGRAM $INPUT
 
-cd scripts
-./cbconvert.sh 20 4 50 50 0
-cd ..
-echo "STAR --- 20 4 50 50 0"
-./star.sh r R ../cbscenarios-star/deep/100/dependencies/program.star ../cbscenarios-star/deep/100/data/input.stream 
+PARAM="20 5 50 50 100"
+echo "STAR --- $PARAM"
+scripts/cbconvert.sh $PARAM
+./star.sh r S $PROGRAM $INPUT
 
-cd scripts
-./cbconvert.sh 20 4 50 50 100
-cd ..
-echo "STAR --- 20 4 50 50 100"
-./star.sh r R ../cbscenarios-star/deep/100/dependencies/program.star ../cbscenarios-star/deep/100/data/input.stream 
-
-cd scripts
-./cbconvert.sh 20 4 100 0 50
-cd ..
-echo "STAR --- 20 4 100 0 50"
-./star.sh r R ../cbscenarios-star/deep/100/dependencies/program.star ../cbscenarios-star/deep/100/data/input.stream 
-
-cd scripts
-./cbconvert.sh 20 4 0 100 50
-cd ..
-echo "STAR --- 20 4 0 100 50"
-./star.sh r R ../cbscenarios-star/deep/100/dependencies/program.star ../cbscenarios-star/deep/100/data/input.stream 
-
+PARAM="20 5 50 50 0"
+echo "STAR --- $PARAM"
+scripts/cbconvert.sh $PARAM
+./star.sh r S $PROGRAM $INPUT
--- scripts/cbconvert.sh
+++ scripts/cbconvert.sh
@@ -9,51 +9,43 @@ EVENT=$5
 
 # === Deep ===
 
-mkdir -p ../../cbscenarios-star/deep/100/data/
-mkdir -p ../../cbscenarios-star/deep/100/dependencies/
-python cbparse.py S $TIME ../../chasebench/scenarios/deep/100/data/ > ../../cbscenarios-star/deep/100/data/input.stream
-python cbparse.py T $WINDOW $BOX $DIAMOND $EVENT ../../chasebench/scenarios/deep/100/dependencies/ > ../../cbscenarios-star/deep/100/dependencies/program.star
+mkdir -p ../cbscenarios-star/deep/100/data/
+mkdir -p ../cbscenarios-star/deep/100/dependencies/
+python scripts/cbparse.py S $TIME ../chasebench/scenarios/deep/100/data/ > ../cbscenarios-star/deep/100/data/input.stream
+python scripts/cbparse.py T $WINDOW $BOX $DIAMOND $EVENT ../chasebench/scenarios/deep/100/dependencies/ > ../cbscenarios-star/deep/100/dependencies/program.star
 
 
-mkdir -p ../../cbscenarios-star/deep/200/data/
-mkdir -p ../../cbscenarios-star/deep/200/dependencies/
-python cbparse.py S $TIME ../../chasebench/scenarios/deep/200/data/ > ../../cbscenarios-star/deep/200/data/input.stream
-python cbparse.py T $WINDOW $BOX $DIAMOND $EVENT ../../chasebench/scenarios/deep/200/dependencies/ > ../../cbscenarios-star/deep/200/dependencies/program.star
+mkdir -p ../cbscenarios-star/deep/200/data/
+mkdir -p ../cbscenarios-star/deep/200/dependencies/
+python scripts/cbparse.py S $TIME ../chasebench/scenarios/deep/200/data/ > ../cbscenarios-star/deep/200/data/input.stream
+python scripts/cbparse.py T $WINDOW $BOX $DIAMOND $EVENT ../chasebench/scenarios/deep/200/dependencies/ > ../cbscenarios-star/deep/200/dependencies/program.star
 
 
-mkdir -p ../../cbscenarios-star/deep/300/data/
-mkdir -p ../../cbscenarios-star/deep/300/dependencies/
-python cbparse.py S $TIME ../../chasebench/scenarios/deep/300/data/ > ../../cbscenarios-star/deep/300/data/input.stream
-python cbparse.py T $WINDOW $BOX $DIAMOND $EVENT ../../chasebench/scenarios/deep/300/dependencies/ > ../../cbscenarios-star/deep/300/dependencies/program.star
 
 # === LUBM ==
 
-mkdir -p ../../cbscenarios-star/LUBM/data/001/
-#mkdir -p ../../cbscenarios-star/LUBM/data/010/
-#mkdir -p ../../cbscenarios-star/LUBM/data/100/
-mkdir -p ../../cbscenarios-star/LUBM/dependencies/
-python cbparse.py S $TIME ../../chasebench/scenarios/LUBM/data/001/ > ../../cbscenarios-star/LUBM/data/001/input.stream
-#python cbparse.py S $TIME ../../chasebench/scenarios/LUBM/data/010/ > ../../cbscenarios-star/LUBM/data/010/input.stream
-#python cbparse.py S $TIME ../../chasebench/scenarios/LUBM/data/100/ > ../../cbscenarios-star/LUBM/data/100/input.stream
-python cbparse.py T $WINDOW $BOX $DIAMOND $EVENT ../../chasebench/scenarios/LUBM/dependencies/ > ../../cbscenarios-star/LUBM/dependencies/program.star
+#mkdir -p ../cbscenarios-star/LUBM/data/001/
+#mkdir -p ../cbscenarios-star/LUBM/dependencies/
+#python scripts/cbparse.py S $TIME ../chasebench/scenarios/LUBM/data/001/ > ../cbscenarios-star/LUBM/data/001/input.stream
+#python cbparse.py T $WINDOW $BOX $DIAMOND $EVENT ../chasebench/scenarios/LUBM/dependencies/ > ../cbscenarios-star/LUBM/dependencies/program.star
 
 
 # === Correctnes scenarios ===
 
-mkdir -p ../../cbscenarios-star/correctness/tgds5/data/
-mkdir -p ../../cbscenarios-star/correctness/tgds5/dependencies/
-python cbparse.py S $TIME ../../chasebench/scenarios/correctness/tgds5/data/ > ../../cbscenarios-star/correctness/tgds5/data/input.stream
-python cbparse.py T $WINDOW $BOX $DIAMOND $EVENT ../../chasebench/scenarios/correctness/tgds5/dependencies/ > ../../cbscenarios-star/correctness/tgds5/dependencies/program.star
+#mkdir -p ../../cbscenarios-star/correctness/tgds5/data/
+#mkdir -p ../../cbscenarios-star/correctness/tgds5/dependencies/
+#python cbparse.py S $TIME ../../chasebench/scenarios/correctness/tgds5/data/ > ../../cbscenarios-star/correctness/tgds5/data/input.stream
+#python cbparse.py T $WINDOW $BOX $DIAMOND $EVENT ../../chasebench/scenarios/correctness/tgds5/dependencies/ > ../../cbscenarios-star/correctness/tgds5/dependencies/program.star
 
-mkdir -p ../../cbscenarios-star/correctness/weak/data/
-mkdir -p ../../cbscenarios-star/correctness/weak/dependencies/
-python cbparse.py S $TIME ../../chasebench/scenarios/correctness/weak/data/ > ../../cbscenarios-star/correctness/weak/data/input.stream
-python cbparse.py T $WINDOW $BOX $DIAMOND $EVENT ../../chasebench/scenarios/correctness/weak/dependencies/ > ../../cbscenarios-star/correctness/weak/dependencies/program.star
+#mkdir -p ../../cbscenarios-star/correctness/weak/data/
+#mkdir -p ../../cbscenarios-star/correctness/weak/dependencies/
+#python cbparse.py S $TIME ../../chasebench/scenarios/correctness/weak/data/ > ../../cbscenarios-star/correctness/weak/data/input.stream
+#python cbparse.py T $WINDOW $BOX $DIAMOND $EVENT ../../chasebench/scenarios/correctness/weak/dependencies/ > ../../cbscenarios-star/correctness/weak/dependencies/program.star
 
-mkdir -p ../../cbscenarios-star/correctness/tgds/data/
-mkdir -p ../../cbscenarios-star/correctness/tgds/dependencies/
-python cbparse.py S $TIME ../../chasebench/scenarios/correctness/tgds/data/ > ../../cbscenarios-star/correctness/tgds/data/input.stream
-python cbparse.py T $WINDOW $BOX $DIAMOND $EVENT ../../chasebench/scenarios/correctness/tgds/dependencies/ > ../../cbscenarios-star/correctness/tgds/dependencies/program.star
+#mkdir -p ../../cbscenarios-star/correctness/tgds/data/
+#mkdir -p ../../cbscenarios-star/correctness/tgds/dependencies/
+#python cbparse.py S $TIME ../../chasebench/scenarios/correctness/tgds/data/ > ../../cbscenarios-star/correctness/tgds/data/input.stream
+#python cbparse.py T $WINDOW $BOX $DIAMOND $EVENT ../../chasebench/scenarios/correctness/tgds/dependencies/ > ../../cbscenarios-star/correctness/tgds/dependencies/program.star
 
 
 
