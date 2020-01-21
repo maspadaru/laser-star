068b3c7fcaed7a89f9b70abb8d5de47d87ec44ac
Mike Spadaru <maspadaru@tutanota.com>
2020-01-21 17:18:26 +0100

    results vlog

--- cbrun.sh
+++ cbrun.sh
@@ -13,155 +13,155 @@ echo " ===================== VS VLog ========================= "
 echo " "
 echo " "
 
-PARAM="7 3 50 50 0"
+PARAM="3 1 50 50 0"
 echo "STAR --- $PARAM"
 scripts/cbconvert.sh $PARAM
 ./star.sh r R $PROGRAM $INPUT
 
-PARAM="7 5 50 50 0"
+PARAM="3 1 50 50 50"
 echo "STAR --- $PARAM"
 scripts/cbconvert.sh $PARAM
 ./star.sh r R $PROGRAM $INPUT
 
-PARAM="7 3 50 50 50"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r R $PROGRAM $INPUT
+#PARAM="7 3 50 50 50"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r R $PROGRAM $INPUT
 
-PARAM="7 5 50 50 50"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r R $PROGRAM $INPUT
+#PARAM="7 5 50 50 50"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r R $PROGRAM $INPUT
 
-echo " "
-echo " "
-echo " ===================== SKOLEM ========================= "
-echo " "
-echo " "
+#echo " "
+#echo " "
+#echo " ===================== SKOLEM ========================= "
+#echo " "
+#echo " "
 
-PARAM="20 1 50 50 0"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r S $PROGRAM $INPUT
+#PARAM="20 1 50 50 0"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r S $PROGRAM $INPUT
 
-PARAM="20 3 50 50 0"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r S $PROGRAM $INPUT
+#PARAM="20 3 50 50 0"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r S $PROGRAM $INPUT
 
-PARAM="20 5 50 50 0"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r S $PROGRAM $INPUT
+#PARAM="20 5 50 50 0"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r S $PROGRAM $INPUT
 
-PARAM="20 10 50 50 0"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r S $PROGRAM $INPUT
+#PARAM="20 10 50 50 0"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r S $PROGRAM $INPUT
 
-PARAM="20 1 50 50 50"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r S $PROGRAM $INPUT
+#PARAM="20 1 50 50 50"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r S $PROGRAM $INPUT
 
-PARAM="20 3 50 50 50"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r S $PROGRAM $INPUT
+#PARAM="20 3 50 50 50"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r S $PROGRAM $INPUT
 
-PARAM="20 5 50 50 50"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r S $PROGRAM $INPUT
+#PARAM="20 5 50 50 50"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r S $PROGRAM $INPUT
 
-PARAM="20 10 50 50 50"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r S $PROGRAM $INPUT
+#PARAM="20 10 50 50 50"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r S $PROGRAM $INPUT
 
-PARAM="20 1 50 50 100"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r S $PROGRAM $INPUT
+#PARAM="20 1 50 50 100"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r S $PROGRAM $INPUT
 
-PARAM="20 3 50 50 100"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r S $PROGRAM $INPUT
+#PARAM="20 3 50 50 100"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r S $PROGRAM $INPUT
 
-PARAM="20 5 50 50 100"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r S $PROGRAM $INPUT
+#PARAM="20 5 50 50 100"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r S $PROGRAM $INPUT
 
-PARAM="20 10 50 50 100"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r S $PROGRAM $INPUT
+#PARAM="20 10 50 50 100"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r S $PROGRAM $INPUT
 
 
-echo " "
-echo " "
-echo " ===================== RESTRICTED ========================= "
-echo " "
-echo " "
+#echo " "
+#echo " "
+#echo " ===================== RESTRICTED ========================= "
+#echo " "
+#echo " "
 
-PARAM="20 1 50 50 0"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r R $PROGRAM $INPUT
+#PARAM="20 1 50 50 0"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r R $PROGRAM $INPUT
 
-PARAM="20 3 50 50 0"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r R $PROGRAM $INPUT
+#PARAM="20 3 50 50 0"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r R $PROGRAM $INPUT
 
-PARAM="20 5 50 50 0"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r R $PROGRAM $INPUT
+#PARAM="20 5 50 50 0"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r R $PROGRAM $INPUT
 
-PARAM="20 10 50 50 0"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r R $PROGRAM $INPUT
+#PARAM="20 10 50 50 0"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r R $PROGRAM $INPUT
 
-PARAM="20 1 50 50 50"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r R $PROGRAM $INPUT
+#PARAM="20 1 50 50 50"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r R $PROGRAM $INPUT
 
-PARAM="20 3 50 50 50"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r R $PROGRAM $INPUT
+#PARAM="20 3 50 50 50"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r R $PROGRAM $INPUT
 
-PARAM="20 5 50 50 50"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r R $PROGRAM $INPUT
+#PARAM="20 5 50 50 50"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r R $PROGRAM $INPUT
 
-PARAM="20 10 50 50 50"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r R $PROGRAM $INPUT
+#PARAM="20 10 50 50 50"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r R $PROGRAM $INPUT
 
-PARAM="20 1 50 50 100"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r R $PROGRAM $INPUT
+#PARAM="20 1 50 50 100"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r R $PROGRAM $INPUT
 
-PARAM="20 3 50 50 100"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r R $PROGRAM $INPUT
+#PARAM="20 3 50 50 100"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r R $PROGRAM $INPUT
 
-PARAM="20 5 50 50 100"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r R $PROGRAM $INPUT
+#PARAM="20 5 50 50 100"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r R $PROGRAM $INPUT
 
-PARAM="20 10 50 50 100"
-echo "STAR --- $PARAM"
-scripts/cbconvert.sh $PARAM
-./star.sh r R $PROGRAM $INPUT
+#PARAM="20 10 50 50 100"
+#echo "STAR --- $PARAM"
+#scripts/cbconvert.sh $PARAM
+#./star.sh r R $PROGRAM $INPUT
